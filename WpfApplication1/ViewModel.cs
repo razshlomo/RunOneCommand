0499c60b5201f4f9929525b82c527a9dc93da64f
Raz Shlomo <razshlomo17@windowslive.com>
2015-05-09 19:49:19 +0300

    first

--- WpfApplication1/ViewModel.cs
+++ WpfApplication1/ViewModel.cs
@@ -50,7 +50,7 @@ namespace WpfApplication1
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
             p.StartInfo.CreateNoWindow = true;
-
+            //razsh
             try
             {
                 p.Start();
